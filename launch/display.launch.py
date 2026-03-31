84a357a965ce2d23626277d818ffac244d837e83
rwelf <rwelfordsmall@gmail.com>
2026-03-31 06:53:39 -0400

    Initial commit

---
+++ launch/display.launch.py
@@ -0,0 +1,39 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('robotic_dog_urdf')
+    urdf_file = os.path.join(package_dir, 'urdf', 'robotic_dog_urdf.urdf')
+    rviz_config_file = os.path.join(package_dir, 'rviz', 'display.rviz')
+
+    with open(urdf_file, 'r') as file:
+        robot_description = file.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument('urdf_file', default_value=urdf_file),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            parameters=[{'robot_description': robot_description}],
+	    arguments=['-d', rviz_config_file]  # Hopefully gives correct config?
+        ),
+    ])
