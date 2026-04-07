852e87a23ee6c44bfa9b500d603aad8d21f08ba2
rwelf <rwelfordsmall@gmail.com>
2026-04-07 01:07:37 -0400

    added 8 dof urdf

---
+++ robotic_dog_urdf_8dof/launch/gazebo.launch.py
@@ -0,0 +1,38 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+
+def generate_launch_description():
+    robot_name_in_model = "robotic_dog_urdf_8dof"
+    urdf_tutorial_path = get_package_share_directory('robotic_dog_urdf_8dof')
+    default_model_path = os.path.join(
+        urdf_tutorial_path, 'urdf', 'robotic_dog_urdf_8dof.urdf')
+
+    with open(default_model_path, 'r') as urdf_file:
+        robot_description = urdf_file.read()
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    launch_gazebo = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([get_package_share_directory(
+            'gazebo_ros'), '/launch', '/gazebo.launch.py']),
+    )
+
+    spawn_entity_node = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', '/robot_description',
+                   '-entity', robot_name_in_model])
+
+    return launch.LaunchDescription([
+        robot_state_publisher_node,
+        launch_gazebo,
+        spawn_entity_node
+    ])
